4ed2207d740a40d7a3a42efc282c5012b8b637fa
christie26 <yoonseo9800@gmail.com>
2025-12-16 22:07:16 +0100

    add print in train.py

--- src/train.py
+++ src/train.py
@@ -15,15 +15,47 @@ def save_model(model, filename):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train MLP model")
-    parser.add_argument("--epochs", type=int, default=100)
-    parser.add_argument("--batch_size", type=int, default=32)
-    parser.add_argument("--lr", type=float, default=0.01)
-    parser.add_argument("--hidden", nargs="+", type=int, default=[5, 10])
-    parser.add_argument("--model", type=str, default="mlp_model.npz")
+    parser.add_argument(
+        "--epochs", type=int, default=100, help="Number of training epochs"
+    )
+
+    parser.add_argument(
+        "--batch_size", type=int, default=32, help="Mini-batch size used for training"
+    )
+
+    parser.add_argument(
+        "--lr", type=float, default=0.01, help="Learning rate for gradient descent"
+    )
+
+    parser.add_argument(
+        "--hidden",
+        nargs="+",
+        type=int,
+        default=[5, 10],
+        help="Sizes of hidden layers (space-separated). Example: --hidden 64 32",
+    )
+
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="mlp_model.npz",
+        help="Path to save or load the trained model",
+    )
     args = parser.parse_args()
 
-    train = np.load("data_train.npz")
-    val = np.load("data_val.npz")
+    print(f"Epochs : {args.epochs}")
+    print(f"Batch size : {args.batch_size}")
+    print(f"Learning rate : {args.lr}")
+    print(f"Sizes of hidden layers : {args.hidden}")
+    print(f"Model path : {args.model}\n")
+
+    train_file = "data_train.npz"
+    validation_file = "data_val.npz"
+    train = np.load(train_file)
+    val = np.load(validation_file)
+    print(f"Train file: {train_file}")
+    print(f"Validation file: {validation_file}\n")
+
     X_train, y_train = train["X"], train["y"]
     X_val, y_val = val["X"], val["y"]
     # print(f"X_val: {X_val}, y_val: {y_val}")
